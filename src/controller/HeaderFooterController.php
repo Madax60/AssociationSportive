810beb88c5e71b9d3b57f88e0f084bd25061535d
thomas <thomas.suzanne@lyceestvincent.fr>
2021-01-07 11:57:24 +0100

    controller footer et header

---
+++ src/controller/HeaderFooterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HeaderFooterController extends AbstractController
+{
+    /**
+     * @Route("/header", name="header")
+     */
+    public function index(): Response
+    {
+        return $this->render('header/index.html.twig', [
+            'controller_name' => 'HeaderFooterController',
+        ]);
+
+        return $this->render('footer/index.html.twig', [
+            'controller_name' => 'FooterController',
+        ]);
+    }
+}
