33ec7d72154d1cd421f1811073bbd7ec7c6f22cb
Tony <tony.nguyen1999i@gmail.com>
2021-03-29 12:18:45 +0200

    UPD: user test user fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -26,12 +26,22 @@ final class UserFixtures extends Fixture
         $userAdmin = new User();
         $password = '123';
         $userAdmin
-            ->setEmail('user-Z@gmail.com')
+            ->setEmail('user-A@gmail.com')
             ->setRoles(array('ROLES_ADMIN'));
 
             // Encode le mot de passe et l'insère dans le champ "password".
             $userAdmin->setPassword($this->encoder->encodePassword($userAdmin, $password));
             $manager->persist($userAdmin);
+
+        $userUser = new User();
+        $password = '123';
+        $userUser
+            ->setEmail('user-U@gmail.com')
+            ->setRoles(array('ROLES_USER'));
+
+            // Encode le mot de passe et l'insère dans le champ "password".
+            $userUser->setPassword($this->encoder->encodePassword($userUser, $password));
+            $manager->persist($userUser);
 
         for ($i = 1; $i < 10; $i++) {
             $user = new User();
